70f676bf4a5c068bcc9e16ae045876ae04867b71
Serina <serina.chhem@gmail.com>
2022-01-24 15:18:23 +0100

    Creation footer

---
+++ web/src/components/Footer/Footer.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
+const FooterContainer = styled.div`
+  height: auto;
+  margin: 0 auto;
+`
+const SocialMediaWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 40%;
+  margin: 0 auto;
+`
+const WebsiteRights = styled.small`
+  color: black;
+  font-family: var(--font);
+  font-size: 0.8rem;
+`
+const SocialIcons = styled.div`
+  width: auto;
+  justify-content: space-evenly;
+  display: flex;
+  align-items: center;
+`
+const SocialIconLink = styled.a`
+  color: black;
+  font-size: 19px;
+`
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <SocialMediaWrap>
+        <WebsiteRights>
+          © {new Date().getFullYear()}, Design and Built by Sérina CHHEM
+        </WebsiteRights>
+        <SocialIcons>
+          <SocialIconLink
+            href="https://github.com/serina-chhem"
+            target="_blank"
+            arial-label="Github"
+          >
+            <FaGithub />
+          </SocialIconLink>
+        </SocialIcons>
+
+        <SocialIcons>
+          <SocialIconLink
+            href="https://www.linkedin.com/in/serinachhem"
+            target="_blank"
+            arial-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </SocialIconLink>
+        </SocialIcons>
+      </SocialMediaWrap>
+    </FooterContainer>
+  )
+}
+
+export default Footer
--- web/src/layouts/MainLayout/MainLayout.js
+++ web/src/layouts/MainLayout/MainLayout.js
@@ -1,11 +1,13 @@
 // import { Link, routes } from '@redwoodjs/router'
+import Footer from 'src/components/Footer/Footer'
 import Header from 'src/components/Header/Header'
 
 const MainLayout = ({ children }) => {
   return (
     <>
-      <Header></Header>
+      <Header />
       <main>{children}</main>
+      <Footer />
     </>
   )
 }
